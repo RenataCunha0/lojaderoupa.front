80f8dd35ee3e51b417b628da2299b36ff78f6b90
RenataCunha0 <renatapatriciacunha@gmail.com>
2020-06-16 23:21:23 +0000

    finalizado

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://lojaderoupa.herokuapp.com/lojaderoupa'
+    baseURL: 'https://lojaderoupa.herokuapp.com'
 });
 
 export default api;
